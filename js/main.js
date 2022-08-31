303502bb3a98f73896ebb72b52d7668c85f7182b
Hwangyunji <105402299+Hwangyunji@users.noreply.github.com>
2022-08-31 10:52:10 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 $(function () {
 
   // 시작시 윈도우 크기 판단 후 스크립트 실행
-function checkDevice() {
-  const breakPoint = 768;
-  let w = window.innerWidth;
-  console.log(w);
+  function checkDevice() {
+    const breakPoint = 768;
+    let w = window.innerWidth;
+    console.log(w);
 
-  if (w >= breakPoint) {
-    console.log("큰화면 코드");
-    $("#gnb .depth1 > li").hover(
-      // mouseover
-      function () {
-        $(".depth2") // 여기만 수정하면 됨 this or 선택자
-          .addClass("on");
-      },
-      // mouseout
-      function () {
-        $(".depth2").removeClass("on");
-      }
-    );
-    
-  } else {
-    console.log("작은화면 코드");
-    $("nav .depth1 > li").click(function () {
-      $(this).children(".depth2").toggleClass("on");
-    });
+    if (w >= breakPoint) {
+      console.log("큰화면 코드");
+      $("#gnb .depth1 > li").hover(
+        // mouseover
+        function () {
+          $(".depth2") // 여기만 수정하면 됨 this or 선택자
+            .addClass("on");
+        },
+        // mouseout
+        function () {
+          $(".depth2").removeClass("on");
+        }
+      );
+
+    } else {
+      console.log("작은화면 코드");
+      $("nav .depth1 > li").click(function () {
+        $(this).children(".depth2").toggleClass("on");
+      });
+    }
   }
-}
 
-$(function () {
-  checkDevice();
-  // const breakPoint = 768;
-
-  $(window).on("resize", function () {
+  $(function () {
     checkDevice();
-  });
-}); // $
+    // const breakPoint = 768;
+
+    $(window).on("resize", function () {
+      checkDevice();
+    });
+  }); // $
 
 
 
-  
+
   $('#family-site-btn').click(
     function () {
       $('.family-site-nav > ul')
@@ -47,31 +47,31 @@ $(function () {
     }
   );
 
-  $("#toggle-btn").click(function(){
-		$("#gnb").addClass("on");
-	});
+  $("#toggle-btn").click(function () {
+    $("#gnb").addClass("on");
+  });
 
-  $("#close-btn").click(function(){
-		$("#gnb").removeClass("on");
-	});
+  $("#close-btn").click(function () {
+    $("#gnb").removeClass("on");
+  });
 
 
 
 });
-  
-   
-  
-  
- var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
-  });
+  },
+});
 
 
 //신제품 배너
@@ -79,23 +79,23 @@ $(function () {
 let num = 0; // 이미지 번호(전역변수)
 
 //공식 = 높이값(세로기준) * 이미지 번호
-function changeslider (img_num) {
-    let img_height = 150 * img_num;
-    let m_img_height = 80 * img_num;
+function changeslider(img_num) {
+  let img_height = 150 * img_num;
+  let m_img_height = 80 * img_num;
 
-// $('.k-slider2 .k-sliders2').css ({
-//     transform: `translateY(${-img_height}px)`
-// })
+  // $('.k-slider2 .k-sliders2').css ({
+  //     transform: `translateY(${-img_height}px)`
+  // })
 
-if (matchMedia("screen and (min-width: 768px)").matches) { 
+  if (matchMedia("screen and (min-width: 768px)").matches) {
     // 768px 이상에서 사용할 JavaScript
-    $('.k-slider2 .k-sliders2').css ({
-        transform: `translateY(${-img_height}px)`
+    $('.k-slider2 .k-sliders2').css({
+      transform: `translateY(${-img_height}px)`
     })
   } else {
     // 768px 미만에서 사용할 JavaScript
-    $('#k-m-slider2').css ({
-        transform: `translateY(${-m_img_height}px)`
+    $('#k-m-slider2').css({
+      transform: `translateY(${-m_img_height}px)`
     })
   }
 
@@ -108,11 +108,11 @@ if (matchMedia("screen and (min-width: 768px)").matches) {
 
 
 //자동재생
-setInterval(function(){
-    num++;
-    if(num > 2) { 
-        num = 0;
-    }
-    // console.log(num)
-    changeslider(num);
+setInterval(function () {
+  num++;
+  if (num > 2) {
+    num = 0;
+  }
+  // console.log(num)
+  changeslider(num);
 }, 3000)
